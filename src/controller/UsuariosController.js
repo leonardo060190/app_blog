4beb20ee49683c869213c4eacae1a599ce2549ae
leonardo060190 <leonardo-morruga@hotmail.com>
2023-08-15 18:06:24 -0300

    ok

--- src/controller/UsuariosController.js
+++ src/controller/UsuariosController.js
@@ -19,140 +19,140 @@ module.exports = {
     },
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-   // busca os Usuários referente ao id informado
-   async buscaid(req, res) {
-    await Usuarios.sequelize.query(`SELECT * FROM usuarios WHERE id = ?`,
-        { replacements: [req.params.id] })
-        .then(([results, metadata]) => {
-            if (results.length === 0) {
-                res.status(404).json({
+    // busca os Usuários referente ao id informado
+    async buscaid(req, res) {
+        await Usuarios.sequelize.query(`SELECT * FROM usuarios WHERE id = ?`,
+            { replacements: [req.params.id] })
+            .then(([results, metadata]) => {
+                if (results.length === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "Usuários não encontrada",
+                    });
+                } else {
+                    res.json({
+                        success: true,
+                        Usuarios: results[0],
+                    });
+                }
+            }).catch((error) => {
+                res.status(500).json({
                     success: false,
-                    message: "Usuários não encontrada",
+                    message: error.message,
                 });
-            } else {
-                res.json({
-                    success: true,
-                    Usuarios: results[0],
-                });
-            }
-        }).catch((error) => {
-            res.status(500).json({
-                success: false,
-                message: error.message,
             });
-        });
-},
-////////////////////////////////////////////////////////////////////////////////////////////////
+    },
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
-// autera os dados que for refente os id informado
-async update(req, res) {
-    await Usuarios.sequelize.query(
-        `UPDATE usuarios SET 
+    // autera os dados que for refente os id informado
+    async update(req, res) {
+        await Usuarios.sequelize.query(
+            `UPDATE usuarios SET 
             nome = ?,
             email = ?,
             updated_at = ? 
         WHERE 
             id = ?`,
-        {
-            replacements: [
-                req.body.nome,
-                req.body.email,
-                new Date(),
-                req.params.id
-            ]
-        }
-    )
-        .then(([results, metadata]) => {
-            if (metadata.affectedRows === 0) {
-                res.status(404).json({
-                    success: false,
-                    message: "Usuário não encontrado",
-                });
-            } else {
-                res.json({
-                    success: true,
-                    message: "Cadastro atualizado com sucesso",
-                });
+            {
+                replacements: [
+                    req.body.nome,
+                    req.body.email,
+                    new Date(),
+                    req.params.id
+                ]
             }
-        }).catch((error) => {
-            res.status(500).json({
-                success: false,
-                message: error.message,
+        )
+            .then(([results, metadata]) => {
+                if (metadata.affectedRows === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "Usuário não encontrado",
+                    });
+                } else {
+                    res.json({
+                        success: true,
+                        message: "Cadastro atualizado com sucesso",
+                    });
+                }
+            }).catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: error.message,
+                });
             });
+
+    },
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //insere um novo ususario na tabela
+    async store(req, res) {
+        // Verifique se o e-mail já está cadastrado
+        const usuarioExistente = await Usuarios.findOne({
+            where: { email: req.body.email }
         });
 
-},
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-//insere um novo ususario na tabela
-async store(req, res) {
-            // Verifique se o e-mail já está cadastrado
-            const usuarioExistente = await Usuarios.findOne({
-                where: { email: req.body.email }
+        if (usuarioExistente) {
+            return res.status(400).json({
+                success: false,
+                message: "E-mail já está cadastrado."
             });
-
-            if (usuarioExistente) {
-                return res.status(400).json({
-                    success: false,
-                    message: "E-mail já está cadastrado."
-                });
-            }
-    await Usuarios.sequelize.query(
-        `INSERT INTO usuarios (
+        }
+        await Usuarios.sequelize.query(
+            `INSERT INTO usuarios (
             nome,
             email,
             created_at,
             updated_at
             )
         VALUES (?, ?, ?, ?)`,
-        {
-            replacements:
-                [
-                    req.body.nome,
-                    req.body.email,
-                    new Date(),
-                    new Date()
-                ]
+            {
+                replacements:
+                    [
+                        req.body.nome,
+                        req.body.email,
+                        new Date(),
+                        new Date()
+                    ]
 
-        }
-    )
-        .then(([results, metadata]) => {
-            res.status(201).json({
-                success: true,
-                message: "Usuário cadastrado com sucesso",
-            });
-        }).catch((error) => {
-            res.status(500).json({
-                success: false,
-                message: error.message,
-            });
-        });
-},
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// deleta o usuarios referente ao id informado
-async delete(req, res) {
-    await Usuarios.sequelize.query(`DELETE FROM usuarios WHERE id = ?`,
-        { replacements: [req.params.id] })
-        .then(([results, metadata]) => {
-            if (metadata.affectedRows === 0) {
-                res.status(404).json({
-                    success: false,
-                    message: "Usuàrio não encontrado",
-                });
-            } else {
-                res.json({
-                    success: true,
-                    message: "Usuário deletado com sucesso",
-                });
             }
-        }).catch((error) => {
-            res.status(500).json({
-                success: false,
-                message: error.message,
+        )
+            .then(([results, metadata]) => {
+                res.status(201).json({
+                    success: true,
+                    message: "Usuário cadastrado com sucesso",
+                });
+            }).catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: error.message,
+                });
             });
-        })
-}
+    },
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // deleta o usuarios referente ao id informado
+    async delete(req, res) {
+        await Usuarios.sequelize.query(`DELETE FROM usuarios WHERE id = ?`,
+            { replacements: [req.params.id] })
+            .then(([results, metadata]) => {
+                if (metadata.affectedRows === 0) {
+                    res.status(404).json({
+                        success: false,
+                        message: "Usuàrio não encontrado",
+                    });
+                } else {
+                    res.json({
+                        success: true,
+                        message: "Usuário deletado com sucesso",
+                    });
+                }
+            }).catch((error) => {
+                res.status(500).json({
+                    success: false,
+                    message: error.message,
+                });
+            })
+    }
 
 
 
