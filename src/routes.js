08ea3414d92cd0c47d425938e711e20f7e020828
leonardo060190 <leonardo-morruga@hotmail.com>
2023-08-15 18:05:20 -0300

    adicionado comentarios

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const UsuariosController = require('./controller/UsuariosController');
-const PostsController = require('./controller/PostsController');
+const express = require('express'); // Importa a biblioteca do express
+const UsuariosController = require('./controller/UsuariosController'); // Importa o arquivo UsuariosController da pasta Controller
+const PostsController = require('./controller/PostsController'); // importa o arquivo PostsController da pasta Controller
 
-const routes = express.Router();
+const routes = express.Router(); 
 
 routes.get('/', (req, res) => {
 res.send('Olá leonardo')
@@ -11,19 +11,23 @@ res.send('Olá leonardo')
 ////////////////////////////////////////////////////////////////
 
 // Rotas da tabela Usuario
-routes.get('/usuarios', UsuariosController.index);
-routes.get('/usuarios/:id', UsuariosController.buscaid);
-routes.put('/usuarios/:id', UsuariosController.update);
-routes.post('/usuarios', UsuariosController.store);
-routes.delete('/usuarios/:id', UsuariosController.delete);
+routes.get('/usuarios', UsuariosController.index); // rota que retorna os dados da tabela Usuarios
+routes.get('/usuarios/:id', UsuariosController.buscaid); // rota que retorna os dados tabela Usuarios pelo id informado
+routes.put('/usuarios/:id', UsuariosController.update); //rota que altera os dados de um usuário pelo id
+routes.post('/usuarios', UsuariosController.store); //rota que adiciona um novo usuário na tabela Usuarios
+routes.delete('/usuarios/:id', UsuariosController.delete); //rota que deleta um usuário pelo id informado
 
 ////////////////////////////////////////////////////////////////
 
 //Rotas da tabela Posts
-routes.get('/posts', PostsController.index);
-routes.get('/posts/:id', PostsController.buscaid);
-routes.post('/posts', PostsController.store);
-routes.put('/posts/:id', PostsController.update);
-routes.delete('/posts/:id', PostsController.delete);
+routes.get('/posts', PostsController.index); // rota que retorna os dados da tabela Posts
+routes.get('/posts/:id', PostsController.buscaid);// rota que retorna os dados tabela Posts pelo id informado
+routes.post('/posts', PostsController.store); //rota que adiciona um novo post na tabela Posts
+routes.put('/posts/:id', PostsController.update); //rota que altera os dados de um post pelo id 
+routes.delete('/posts/:id', PostsController.delete); //rota que deleta um post pelo id informado
+
+////////////////////////////////////////////////////////////////
+
+
 module.exports = routes;// exporta as rotas para do aplicação
 
